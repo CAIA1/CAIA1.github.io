d0ad89439ab1799444b28122715ac07897ee79fc
CAIA1 <kaisa.kuos@gmail.com>
2023-05-25 20:28:40 +0400

    fix nav-menu not being hidden in small screen sizes

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const menuIcon = document.getElementById('menu-icon');
-const navMenu = document.querySelector('.nav-menu');
-const navLinks = document.querySelectorAll('.nav-item');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuIcon = document.getElementById('menu-icon');
+  const navMenu = document.querySelector('.nav-menu');
+  const navLinks = document.querySelectorAll('.nav-item a');
 
 
-menuIcon.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-}); 
+  menuIcon.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  }); 
 
 
-navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    navMenu.classList.remove('active');
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      navMenu.classList.remove('active');
+    });
   });
 });
